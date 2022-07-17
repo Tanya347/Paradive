810a5809506ef5688f2393449744b287b8124b49
Kanishka Panwar <kanishkap2002@gmail.com>
2022-07-17 23:47:34 +0530

    preloader added

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,63 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
-import SearchPage from "./pages/SearchPage/SearchPage"
-import Login from "./pages/Login/Login"
-import Register from "./pages/Register/Register"
+import SearchPage from "./pages/SearchPage/SearchPage";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import Post from "./pages/post/Post";
 import CreatePost from "./pages/createPost/CreatePost";
 // import EditPost from "./pages/editPost/EditPost";
-import { useContext } from "react"
-import { AuthContext } from "./context/authContext"
+import { useContext, useState } from "react";
+import { AuthContext } from "./context/authContext";
 // import { Navigate } from "react-router-dom"
 import Activity from "./pages/Activity/Activity";
 import AllActivity from "./pages/allActivity/AllActivity";
 
 function App() {
-  const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
 
   const ProtectedRoute = ({ children }) => {
     if (!user) {
-      return <Login title="Login to Create Post" link="/new" />
-    }
-    else {
+      return <Login title="Login to Create Post" link="/new" />;
+    } else {
       return children;
     }
+  };
+
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById("spinner");
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none";
+      setLoading(false);
+    }, 2000);
   }
 
   return (
-    <BrowserRouter>
-      <Routes>
-
-
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login title="Welcome Back!" link="/" />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/:id" element={<Post />} />
-        {/* <Route path="/:id/edit" element={<EditPost />} /> */}
-        <Route path="/activity/:id" element={<Activity />} />
-        <Route path="/new" element={
-          <ProtectedRoute>
-            <CreatePost />
-          </ProtectedRoute>
-        }
-        />
-        <Route path="/explore" element={<SearchPage />} />
-        <Route path="/all" element={<AllActivity />} />
-      </Routes>
-    </BrowserRouter>
+    !loading && (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/login"
+            element={<Login title="Welcome Back!" link="/" />}
+          />
+          <Route path="/register" element={<Register />} />
+          <Route path="/:id" element={<Post />} />
+          {/* <Route path="/:id/edit" element={<EditPost />} /> */}
+          <Route path="/activity/:id" element={<Activity />} />
+          <Route
+            path="/new"
+            element={
+              <ProtectedRoute>
+                <CreatePost />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/explore" element={<SearchPage />} />
+          <Route path="/all" element={<AllActivity />} />
+        </Routes>
+      </BrowserRouter>
+    )
   );
 }
 
